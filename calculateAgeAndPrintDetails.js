65ede3218dc35d238d2d12b251d782f462de13be
Swagata Chakraborty <cswagata@IN-swagata.local>
2018-10-22 08:34:09 +0530

    main() is modified. Need to be tested.

--- calculateAgeAndPrintDetails.js
+++ calculateAgeAndPrintDetails.js
@@ -1,13 +1,17 @@
-const calculateAgelib = require(./calculateAgelib.js);
+let {computeAge,split,splitName} = require("./calculateAgelib.js");
+
 const main = function () {
   let name = process.argv[2];
   let dateOfBirth = process.argv[3];
   let address = process.argv[4];
+  name = splitName(name);
+  dateOfBirth = split(dateOfBirth);
+  let age = computeAge(dateOfBirth);
   let bioData = {
-    name : calculateAgelib.createNameObject(name),
-    dateOfBirth : splitdateOfBirth(dateOfBirth),
+    name :name,
+    dateOfBirth : dateOfBirth,
     address : address,
-    age : calculateAgelib.computeAge(dateOfBirth)
+    age : age
   };
   printDetails( bioData );
 }
